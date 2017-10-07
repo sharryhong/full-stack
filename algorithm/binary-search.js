81ab02ce6d23c452bfc55f63f0962ec23266bb4d
sharryhong <kshopzoa15@gmail.com>
2017-10-07 21:54:14 +0900

    이진탐색 응용

--- algorithm/binary-search.js
+++ algorithm/binary-search.js
@@ -3,13 +3,16 @@
 var doSearch = function(array, targetValue) {
 	var min = 0;
 	var max = array.length - 1;
-    var guess;
-    while(max >= min) {
-        guess = Math.floor((min + max) / 2);
-        if(array[guess] === targetValue) { return guess; }
-        else if(array[guess] < targetValue) { min = guess + 1; }
-        else { max = guess - 1; }
-    }
+  var guess;
+  var n = 0;
+  while(max >= min) {
+    guess = Math.floor((min + max) / 2);
+    if(array[guess] === targetValue) { console.log(n+'번째에 찾음'); return guess; }
+    else if(array[guess] < targetValue) { min = guess + 1; }
+    else { max = guess - 1; }
+    n++;
+  }
+
 	return -1;
 };
 
@@ -17,4 +20,4 @@ var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 var targetValue = 73;
 var result = doSearch(primes, targetValue);
-console.log(result+'번째에 '+ targetValue +'이/가 있습니다.')
+console.log(result+' Array index에 '+ targetValue +'이/가 있습니다.')
