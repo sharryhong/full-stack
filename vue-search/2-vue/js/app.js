eae7b87504566c34a4159170fda096c5f809b29d
sharryhong <kshopzoa15@gmail.com>
2018-11-11 23:29:46 +0900

    주석

--- vue-search/2-vue/js/app.js
+++ vue-search/2-vue/js/app.js
@@ -1,7 +1,7 @@
 // vue instance
-new Vue({
-    el: '#app', // vue 인스턴스가 html의 어느부분에 마운트될 것 인가
-    data: {
-        msg: 'Hello World'
+new Vue({ // 인자들 el, data
+    el: '#app', // vue 인스턴스가 html의 어느부분에 마운트될 것 인지 설정
+    data: {     
+        msg: 'Hello World' // msg변수 선언
     }
 })
