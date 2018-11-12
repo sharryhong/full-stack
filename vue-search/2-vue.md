20e1650c8672542e42bf3f7ec20e1846a2ab2b04
sharryhong <kshopzoa15@gmail.com>
2018-11-12 16:03:14 +0900

    vue-추천검색, 최근검색

--- vue-search/2-vue.md
+++ vue-search/2-vue.md
@@ -3,7 +3,7 @@
 ### 디렉티브
 #### [v-model](https://kr.vuejs.org/v2/guide/forms.html)
 - 양방향 데이터 바인딩 지원
-- 예) `v-model:value="query"`
+- 예) `v-model:value="query"`, `v-bind:class="{active: tab === selectedTab}"``
 
 #### [v-bind](https://kr.vuejs.org/v2/guide/class-and-style.html)
 - html attribute의 값을 바인딩
@@ -19,3 +19,31 @@
 #### [v-on](https://kr.vuejs.org/v2/guide/events.html)
 - 이벤트 핸들링 (DOM에서 일어나는 이벤트를 리슨)
 - `v-on:submit.prevent="onSubmit"` : `event.preventDefault()` 역할
+
+### Vue instance 인자들
+- `el` : vue 인스턴스가 html의 어느부분에 마운트될 것 인지 설정
+- `data`
+- `created` : vue 인스턴스가 생성될 때 실행 ([인스턴스 라이프사이클참고](https://kr.vuejs.org/v2/guide/instance.html#%EB%9D%BC%EC%9D%B4%ED%94%84%EC%82%AC%EC%9D%B4%ED%81%B4-%EB%8B%A4%EC%9D%B4%EC%96%B4%EA%B7%B8%EB%9E%A8))
+- `methods` : 메서드들 
+
+```
+// vue instance
+new Vue({       // 인자들 el, data
+    el: '#app', // vue 인스턴스가 html의 어느부분에 마운트될 것 인지 설정
+    data: {
+        msg: 'Hello World',  // msg변수 선언
+        submitted: false,    // submit했는지
+        searchResult: [],    // 검색결과
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: ''
+    },
+    created() { // vue 인스턴스가 생성될 때 실행
+        this.selectedTab = this.tabs[0]
+    },
+    methods: {
+        onSubmit() {
+            console.log('onSubmit()');
+        }
+    }
+})
+```
--- vue-search/2-vue/js/app.js
+++ vue-search/2-vue/js/app.js
@@ -4,10 +4,15 @@ import SearchModel from './models/SearchModel.js'
 new Vue({       // 인자들 el, data
     el: '#app', // vue 인스턴스가 html의 어느부분에 마운트될 것 인지 설정
     data: {
-        msg: 'Hello World', // msg변수 선언
-        submitted: false,   // submit했는지
-        query: '',          // 검색어
-        searchResult: []    // 검색결과
+        msg: 'Hello World',  // msg변수 선언
+        submitted: false,    // submit했는지
+        query: '',           // 검색어
+        searchResult: [],    // 검색결과
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: ''
+    },
+    created() { // vue 인스턴스가 생성될 때 실행
+        this.selectedTab = this.tabs[0]
     },
     methods: {
         // 검색어 입력후 submit
@@ -15,25 +20,27 @@ new Vue({       // 인자들 el, data
             console.log('onSubmit()');
             this.search()
         },
-
         // 검색어 입력 x버튼 클릭시
         onReset() {
             console.log('onReset()');
             this.query = '' // 뷰 인스턴스(this)의 데이터(query) 값 초기화
+            this.searchResult = []
             this.submitted = false
         },
-
         // 검색어 입력시
         onKeyup() {
             if(!this.query.length) this.onReset()
         },
-
         // 검색결과
         search() {
             SearchModel.list().then(data => {
                 this.submitted = true
                 this.searchResult = data
             })
+        },
+        // 탭
+        onClickTab(tab) {
+            this.selectedTab = tab
         }
     }
 })
