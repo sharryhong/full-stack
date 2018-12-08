26bc5559d03206c1b4e30985892ba8587d218922
sharryhong <kshopzoa15@gmail.com>
2018-12-09 00:01:22 +0900

    dragra library

--- vue-trello/src/components/Board.vue
+++ vue-trello/src/components/Board.vue
@@ -21,6 +21,8 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List.vue'
+import dragula from 'dragula'
+import 'dragula/dist/dragula.css'
 
 export default {
     components: {
@@ -29,7 +31,8 @@ export default {
     data() {
         return {
             bid: 0,
-            loading: false
+            loading: false,
+            dragulaCards: null
         }
     },
     computed: {
@@ -40,9 +43,45 @@ export default {
     created() {
         this.fetchData()
     },
+    updated() {
+      // 드래그앤드롭: Board컴포넌트 내의 자식 컴포넌트들이 모두 렌더링 된 후
+      // 자식 컴포넌트가 모두 마운트된 시점인 updated에서 실행 해준다.
+      if(this.dragulaCards) this.dragulaCards.destroy() // 우선 사용된 드래그인드롭 이벤트 제거
+
+      this.dragulaCards = dragula([
+        ...Array.from(this.$el.querySelectorAll('.card-list'))
+      ]).on('drop', (el, wrapper, target, siblings) => {
+        const targetCard = {
+          id: el.dataset.cardId * 1, // 숫자로 바꿈
+          pos: 65535
+        }
+        let prevCard = null
+        let nextCard = null
+        Array.from(wrapper.querySelectorAll('.card-item'))
+          .forEach((el, idx, arr) => {
+            const cardId = el.dataset.cardId * 1
+            if(cardId == targetCard.id) {
+              prevCard = idx > 0 ? { // 맨 앞이 아닌경우 prevCard 셋팅
+                id: arr[idx -1].dataset.cardId * 1,
+                pos: arr[idx -1].dataset.cardPos * 1
+              } : null // 맨 앞인 경우 prevCard는 없음
+              nextCard = idx < arr.length - 1 ? { // 마지막이 아닌경우 nextCard 셋팅
+                id: arr[idx +1].dataset.cardId * 1,
+                pos: arr[idx + 1].dataset.cardPos * 1
+              } : null // 맨 마지막인 경우 nextCard는 없음
+            }
+          })
+
+          if(!prevCard && nextCard) targetCard.pos = nextCard.pos / 2 // 맨 앞인 경우
+          else if(!nextCard && prevCard) targetCard.pos = prevCard.pos * 2 // 맨 뒤인 경우
+          else if(prevCard && nextCard) targetCard.pos = (prevCard.pos + nextCard.pos) / 2 // 중간인 경우
+          this.UPDATE_CARD(targetCard)
+      })
+    },
     methods: {
         ...mapActions([
-            'FETCH_BOARD'
+            'FETCH_BOARD',
+            'UPDATE_CARD'
         ]),
         fetchData() {
             this.loading = true
