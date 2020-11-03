5dafc80de6a6672d44ad60e94fd216c737cd8b85
sharryhong <kshopzoa15@gmail.com>
2020-11-03 12:37:41 +0900

    [nuxt] html 추가

--- nuxt/real-world-nuxt/pages/event/_id.vue
+++ nuxt/real-world-nuxt/pages/event/_id.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <h1>{{ event.title }}</h1>
+    <div class="event-header">
+      <span class="eyebrow"> @{{ event.time }} on {{ event.date }} </span>
+      <h1 class="title">
+        {{ event.title }}
+      </h1>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : "" }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+
+    <span name="map">
+      <h2>Location</h2>
+    </span>
+
+    <address>{{ event.location }}</address>
+
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">
+        {{ event.attendees ? event.attendees.length : 0 }}
+      </span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
   </div>
 </template>
 
