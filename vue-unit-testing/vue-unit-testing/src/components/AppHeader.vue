9d8ee3fec52db4099d19c0efce8b76573a9a7a69
sharryhong <kshopzoa15@gmail.com>
2020-11-11 19:35:53 +0900

    [unit-testing] jest 첫번째

--- vue-unit-testing/vue-unit-testing/src/components/AppHeader.vue
+++ vue-unit-testing/vue-unit-testing/src/components/AppHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button v-show="loggedIn">Logout</button>
+    <!-- 테스트할 output 항목
+    렌더링 된 출력 : <button> loggedIn 입력 값에 따라 버튼이 DOM에 표시되는지 여부 -->
+    <button v-show="loggedIn">Logout</button> 
   </div>
 </template>
 
@@ -8,7 +10,7 @@
 export default {
   data() {
     return {
-      loggedIn: false
+      loggedIn: false // 테스트할 input 항목 
     };
   }
 };
---
+++ vue-unit-testing/vue-unit-testing/tests/unit/AppHeader.spec.js
@@ -0,0 +1,26 @@
+import AppHeader from '@/components/AppHeader'
+import { mount } from '@vue/test-utils'
+
+// describe : 관련 테스트를 그룹화. 테스트가 하나뿐이면 describe 블록으로 래핑 할 필요가 없다.
+describe('AppHeader', () => {
+  // https://jestjs.io/docs/en/api#testname-fn-timeout
+  // test(테스트 를 정의하는 문자열, 실제 테스트 논리가 이동할 함수)
+  test('If user is not logged in, do not show logout button', () => {
+    // test body : 논리 
+    const wrapper = mount(AppHeader) // 컴포넌트 마운트 
+    // expect() : 테스트가 반환 할 것으로 예상 하는 내용이 실제로 반환 된 내용과 일치하는지 여부를 결정
+    // expect(theResult).toBe(true)
+    // https://jestjs.io/docs/en/expect
+    // expect(true).toBe(true) // $ yarn test:unit 로 일단 테스트용  
+    expect(wrapper.find('button').isVisible()).toBe(false)
+  })
+
+  test('If user is logged in, show logout button', async () => {
+    const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: true }) // 현재 값은 false이므로 여기에서는 true로 바꿔준다. 
+
+    // true로 바뀐 뒤에 실행되어야하므로, async await 문법 필요 
+    await wrapper.vm.$nextTick() 
+    expect(wrapper.find('button').isVisible()).toBe(true)
+  })
+});
