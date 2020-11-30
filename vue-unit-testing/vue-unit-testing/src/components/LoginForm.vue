5ae0bdd350a71557c6f63d154a0d1c4e94380220
sharryhong <kshopzoa15@gmail.com>
2020-11-30 17:00:43 +0900

    [jest] emit test setting

---
+++ vue-unit-testing/vue-unit-testing/src/components/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <input type="text" v-model="name">
+    <button type="button" @click="onSubmit">Login</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: ''
+      }
+    },
+    methods: {
+      onSubmit() {
+        this.$emit('loginSubmitted', { name: this.name })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-unit-testing/vue-unit-testing/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <LoginForm @loginSubmitted="onLoginSubmitted" />
+  </div>
+</template>
+
+<script>
+  import LoginForm from '@/components/LoginForm'
+  export default {
+    components: {
+      LoginForm
+    },
+    methods: {
+      onLoginSubmitted(data) {
+        console.log('onLoginSubmitted', data);
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
