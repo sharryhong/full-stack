92bd5f84a0ed61706d8310a4104f1a97b103e9c7
sharryhong <kshopzoa15@gmail.com>
2020-12-02 18:22:37 +0900

    [jest] API call test setting

---
+++ vue-unit-testing/vue-unit-testing/src/components/MessageDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-if="error" data-testId="message-error">{{ error }}</p>
+    <p v-else data-testId="message">{{ message.text }}</p>
+  </div>
+</template>
+
+<script>
+  import { getMessage } from '@/service/axios.js'
+
+  export default {
+    data() {
+      return {
+        message: {},
+        error: null
+      }
+    },
+    async created() {
+      try {
+        this.message = await getMessage()
+      } catch (err) {
+        this.error = 'Oops! Something went wrong.'
+      }
+    }
+  }
+</script>
