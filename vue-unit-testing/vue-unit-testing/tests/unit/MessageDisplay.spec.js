13578224a8a55c3d8b54eb24a7064697ad44d264
sharryhong <kshopzoa15@gmail.com>
2020-12-11 17:48:26 +0900

    [jest] api call test - step2

--- vue-unit-testing/vue-unit-testing/tests/unit/MessageDisplay.spec.js
+++ vue-unit-testing/vue-unit-testing/tests/unit/MessageDisplay.spec.js
@@ -1,14 +1,26 @@
 import MessageDisplay from '@/components/MessageDisplay'
 import { mount } from '@vue/test-utils';
+import { getMessage } from '@/services/axios';
+
+jest.mock('@/services/axios')
 
 describe('MessageDisplay', () => {
   it('Calls getMessage and display message', async () => {
-    // API call 가정
+    // API call 모의(Mock)
+    const mockMessage = "Hello from the db!"
+    getMessage.mockResolvedValueOnce({ "text": mockMessage })
+
     const wrapper = mount(MessageDisplay)
+
+    // wait for promise to resolve
+
+    // check that call happened once
+
+    // check that component display message
   })
 
   it('Displays an error when getMessage call fails', async () => {
-    // API call 가정
+    // API call 모의(Mock)
     const wrapper = mount(MessageDisplay)
   })
 })
