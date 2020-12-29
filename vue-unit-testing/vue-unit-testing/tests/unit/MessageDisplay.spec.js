f4c1c9cbe4d174f80cc8d6f8967b9cbe325f2657
sharryhong <kshopzoa15@gmail.com>
2020-12-29 11:18:43 +0900

    [jest] resolved, rejected

--- vue-unit-testing/vue-unit-testing/tests/unit/MessageDisplay.spec.js
+++ vue-unit-testing/vue-unit-testing/tests/unit/MessageDisplay.spec.js
@@ -4,23 +4,34 @@ import { getMessage } from '@/services/axios';
 import flushPromises from "flush-promises";
 
 jest.mock('@/services/axios')
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('MessageDisplay', () => {
   it('Calls getMessage and display message', async () => {
     // API call 모의(Mock)
     const mockMessage = "Hello from the db!"
-    getMessage.mockResolvedValueOnce({ "text": mockMessage })
+    getMessage.mockResolvedValueOnce({ text: mockMessage }) // 성공
     const wrapper = mount(MessageDisplay)
     // wait for promise to resolve
     await flushPromises();
-
     // check that call happened once
-
+    expect(getMessage).toHaveBeenCalledTimes(1);
     // check that component display message
+    const message = wrapper.find('[data-testId="message"]').element.textContent
+    expect(message).toEqual(mockMessage)
   })
 
   it('Displays an error when getMessage call fails', async () => {
-    // API call 모의(Mock)
+    const mockError = "Oops! Something went wrong.";
+    getMessage.mockRejectedValueOnce(mockError); // 실패 
     const wrapper = mount(MessageDisplay)
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+    const displayedError = wrapper.find('[data-testid="message-error"]').element.textContent;
+    expect(displayedError).toEqual(mockError);
   })
 })
+
