1755b6503de6786438d05c5d820ec2dc188ea000
sharryhong <kshopzoa15@gmail.com>
2021-03-08 18:25:56 +0900

    [react] state

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,76 +1,31 @@
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4.6,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.7,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.6,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.8,
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }))
   }
-];
 
-function renderFood(dish) {
-  return (
-    <Food
-      name={dish.name}
-      image={dish.image}
-      rating={dish.rating}
-      key={dish.id}
-    />
-  );
-}
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }))
+  }
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <img src={image} alt={name} />
-      <span>{rating}</span>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div className="App">
-      { foodILike.map(renderFood)}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <div>Number: {this.state.count}</div>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.minus}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
