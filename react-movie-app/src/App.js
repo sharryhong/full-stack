ccc26a2ffb1e15f21c410c8baebe326d67825449
sharryhong <kshopzoa15@gmail.com>
2021-03-10 15:27:25 +0900

    [react] movie component

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends Component {
   state = {
@@ -8,17 +9,27 @@ class App extends Component {
   };
   getMovies = async () => {
     // response data: data.data.movies로 오는 상황 
-    const { data: { data: { movies }}} = await axios.get('http://yts-proxy.now.sh/list_movies.json ')
+    const { data: { data: { movies } } } = await axios.get('http://yts-proxy.now.sh/list_movies.json?sort_by=rating')
     this.setState({ movies, isLoading: false })
   }
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        { isLoading ? "Loading..." : "We are ready" }
+        { isLoading 
+        ? "Loading..." 
+        : movies.map(movie => (
+          <Movie 
+            key={movie.id} 
+            id={movie.id} 
+            title={movie.title} 
+            year={movie.year} 
+            summary={movie.summary} 
+            poster={movie.small_cover_image} 
+          />))}
       </div>
     );
   }
