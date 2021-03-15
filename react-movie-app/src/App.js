5d5d18d1df1e9c1f8e022d8addb4c6759e586323
sharryhong <kshopzoa15@gmail.com>
2021-03-15 18:12:04 +0900

    [react] navigation

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <HashRouter>
-    <Route path="/" exact={true} component={ Home } />
-    <Route path="/about" component={ About } />
-  </HashRouter>
+  return (
+    <>
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={ Home } />
+      <Route path="/about" component={ About } />
+    </BrowserRouter>
+    <footer>footer</footer>
+    </>
+  )
 }
 
 export default App;
