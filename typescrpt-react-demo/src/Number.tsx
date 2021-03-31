4bdf5476a9f69e9f9c297df4dc53ebe32634473d
sharryhong <kshopzoa15@gmail.com>
2021-03-31 16:23:27 +0900

    [typescript-react] Styled Components 적용

--- typescrpt-react-demo/src/Number.tsx
+++ typescrpt-react-demo/src/Number.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-interface IContainerProps {
-  isBlue: boolean
-}
-
-const Container = styled.span<IContainerProps>`
-  color: ${props => props.isBlue ? 'red' : '#333'}
+const Container = styled.span<{isBlue: boolean}>`
+  color: ${props => props.isBlue ? props.theme.redColor : '#333'}
 `;
 
 interface IProps {
