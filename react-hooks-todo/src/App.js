e6af38b83f0a9cb75316c5c6120eb6c390cc4eca
sharryhong <kshopzoa15@gmail.com>
2021-04-10 17:18:41 +0900

    [react-hooks-todo] useState

--- react-hooks-todo/src/App.js
+++ react-hooks-todo/src/App.js
@@ -3,12 +3,23 @@ import List from "./List";
 
 const App = () => {
   const [todos, setTodos] = useState(['1']);
+  const [newTodo, setNewTodo] = useState();
+
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <>
       <h1>Todo App</h1>
       <form action="">
-        <input type="text" name="" />
-        <button>할일 추가</button>
+        <input type="text" name="" onChange={changeInputData} />
+        <button onClick={addTodo}>할일 추가</button>
       </form>
 
       <List todos={todos} />
--- react-hooks-todo/src/List.jsx
+++ react-hooks-todo/src/List.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const List = ({todos}) => {
-const todosList = todos.map(todo => <li>{todo}</li>)
+const todosList = todos.map((todo, index) => <li key={index}>{todo}</li>)
   return (
     <ul>
       {todosList}
