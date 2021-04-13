c54e72c0891c360ef087a0cfb95a4794b15fafca
sharryhong <kshopzoa15@gmail.com>
2021-04-13 19:52:37 +0900

    [react-hooks-todo] useReducer 셋팅중

--- react-hooks-todo/src/TodoStore.js
+++ react-hooks-todo/src/TodoStore.js
@@ -1,35 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import List from "./List";
 import Header from './Header';
 import Form from './Form';
 import useFetch from "./useFetch";
 
 // 상위 store역할 
-export const TodoContext = React.createContext()
+export const TodoContext = React.createContext();
+
+const todoReducer = (todos, {type, payload}) => { // parameter: state, action {type, payload}
+  switch (type) {
+    case 'ADD_TODO':
+      return;
+    case 'SET_INIT_DATA':
+      return payload;
+    case 'CHANGE_TODO_STATUS':
+      return;
+    default: 
+      break;
+  }
+}
 
 const TodoStore = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(todoReducer, []); // 두번째 argument는 초기값
 
-  const isLoaded = useFetch(setTodos, "http://localhost:9000/todo"); 
-  
+  const setInitData = (initData) => {
+    // dispatch로 action을 발생시킨다. 
+    dispatch({ type: "SET_INIT_DATA", payload: initData });
+  }
+
+  const isLoaded = useFetch(setInitData, "http://localhost:9000/todo");
+
   const addTodo = (newTodo) => {
     setTodos([
       ...todos,
       { title: newTodo, id: todos.length + 1, status: "todo" },
-    ]); 
-  }
+    ]);
+  };
 
   const changeTodoStatus = (id) => {
-    const updateTodos = todos.map(todo => {
-      if(todo.id === id) {
-        if (todo.status === 'done') todo.status = 'todo'
-        else todo.status = 'done'
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        if (todo.status === "done") todo.status = "todo";
+        else todo.status = "done";
       }
       return todo;
-    })
+    });
 
     setTodos(updateTodos);
-  }
+  };
 
   useEffect(() => {
     console.log("todos", todos);
