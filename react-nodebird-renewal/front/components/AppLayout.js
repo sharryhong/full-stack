c6c66535886204a3c52958c91f6d6ce897ef4c9e
sharryhong <kshopzoa15@gmail.com>
2021-07-21 18:33:33 +0900

    [react-nodebird] _app, head

--- react-nodebird-renewal/front/components/AppLayout.js
+++ react-nodebird-renewal/front/components/AppLayout.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Menu } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
     <>
-      <div>
-        <Link href="/"><a>Nodebird</a></Link>
-        <Link href="/profile"><a>Profile</a></Link>
-        <Link href="/signup"><a>Signup</a></Link>
-      </div>
-      <div>{ children }</div>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>Nodebird</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="signup">
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <div>{children}</div>
     </>
-  )
+  );
 }
 
 AppLayout.propTypes = {
