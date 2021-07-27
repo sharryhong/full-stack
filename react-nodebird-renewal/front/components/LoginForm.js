49738cfedea360444d1b9de9b88602106c6e8754
sharryhong <kshopzoa15@gmail.com>
2021-07-27 16:27:47 +0900

    [react-nodebird] Signup

--- react-nodebird-renewal/front/components/LoginForm.js
+++ react-nodebird-renewal/front/components/LoginForm.js
@@ -1,23 +1,17 @@
-import React, { useState, useCallback } from "react";
+import React, { useCallback } from "react";
 import { Form, Input, Button } from "antd";
 import Link from 'next/link';
 import styled from 'styled-components';
 import PropTypes from "prop-types";
+import useInput from '../hooks/useInput';
 
 const ButtonWrap = styled.div`
   margin-top: 10px;
 `
 
 const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onChangeId = useCallback((e) => {
-    setId(e.target.value);
-  }, []);
-  const onChangePassword = useCallback((e) => {
-    setPassword(e.target.value);
-  }, []);
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
