cdcabb813d488e960acd5dede5eff1a28ba8796d
sharryhong <kshopzoa15@gmail.com>
2021-07-28 16:31:37 +0900

    [vue-ts] init

---
+++ vue-typescript-todo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Vue Todo with TS</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
