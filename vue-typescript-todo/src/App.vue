89ffa9ed3fa9bff3c8777821d747b1ac0ba1ba24
sharryhong <kshopzoa15@gmail.com>
2021-07-29 10:47:19 +0900

    [vue-ts] TodoInput

--- vue-typescript-todo/src/App.vue
+++ vue-typescript-todo/src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <h1>Vue Todo with TS</h1>
+    <TodoInput :item="todoText" @input="updateTodoText" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoInput from "@/components/TodoInput.vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+  },
+});
 </script>
 
 <style scoped></style>
---
+++ vue-typescript-todo/src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button type="button" @click="addTodo">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    handleInput(event: any) {
+      this.$emit("input", event.target.value);
+    },
+    addTodo() {
+      console.log("item", this.item);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
