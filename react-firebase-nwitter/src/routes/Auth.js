fbb9f5493adc527ae0de0b59a4b98b8b58c57e9a
sharryhong <kshopzoa15@gmail.com>
2021-10-28 19:36:31 +0900

    login form

--- react-firebase-nwitter/src/routes/Auth.js
+++ react-firebase-nwitter/src/routes/Auth.js
@@ -1,4 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (event) => {
+    const {target: {name, value}} = event;
+    if(name === 'email') { 
+      setEmail(value);
+    } else if(name === 'password') { 
+      setPassword(value);
+    }
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="email" type="email" placeholder="email" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
+        <input type="submit" value="Login" />
+      </form>
+      <div>
+        <button type="button">Continue with Google</button>
+        <button type="button">Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
