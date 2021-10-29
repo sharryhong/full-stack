fbe3ac11b4d75bb55acbf0c3a0c8cd7a2f54383b
sharryhong <kshopzoa15@gmail.com>
2021-10-29 18:44:53 +0900

    파이어베이스 로그인연결

--- react-firebase-nwitter/src/components/App.js
+++ react-firebase-nwitter/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { AppRouter } from 'components/Router';
 import { authService } from 'fbase';
-console.log('authService', authService);
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
--- react-firebase-nwitter/src/routes/Auth.js
+++ react-firebase-nwitter/src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { authService } from 'fbase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (event) => {
     const {target: {name, value}} = event;
@@ -12,8 +15,22 @@ const Auth = () => {
       setPassword(value);
     }
   }
-  const onSubmit = (event) => {
+  const onSubmit = async(event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account 
+        data = await createUserWithEmailAndPassword(authService, email, password)
+      } else {
+        // login 
+        data = await signInWithEmailAndPassword(authService, email, password)
+      }
+      console.log('data', data);
+      
+    } catch(err) {
+      console.log(err);
+    }
   }
 
   return (
@@ -21,7 +38,7 @@ const Auth = () => {
       <form onSubmit={onSubmit}>
         <input name="email" type="email" placeholder="email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
-        <input type="submit" value="Login" />
+        <input type="submit" value={newAccount ? "Create Account" : "Login"} />
       </form>
       <div>
         <button type="button">Continue with Google</button>
