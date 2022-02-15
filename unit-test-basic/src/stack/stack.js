168b0eea1408a89697bd25f8a8c6e464751c4227
sharryhong <kshopzoa15@gmail.com>
2022-02-15 09:51:39 +0900

    stack 리팩토링

--- unit-test-basic/src/stack/stack.js
+++ unit-test-basic/src/stack/stack.js
@@ -1,24 +1,30 @@
 class Stack {
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null;
   }
   size() {
-    return this.array.length;
+    return this._size;
   }
   push(item) {
-    this.array.push(item);
+    const node = { item, next: this.head };
+    this.head = node;
+    this._size++;
   }
   pop() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array.pop();
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.item;
   }
   peek() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array[this.array.length - 1];
+    return this.head.item;
   }
 }
 
